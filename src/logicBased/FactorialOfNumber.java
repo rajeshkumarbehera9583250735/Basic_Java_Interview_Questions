e41f4a197c4c90c8b713f8d34194b8dc974ac343
Rajesh Kumar Behera <rajeshkumarbeheracool@gmail.com>
2025-07-06 00:30:18 +0530

    Factorial Of A Number

---
+++ src/logicBased/FactorialOfNumber.java
@@ -0,0 +1,25 @@
+/*
+
+Problem: Write a Java program to find the factorial of a given number.
+
+*/
+package logicBased;
+import java.util.Scanner;
+
+public class FactorialOfNumber {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int fact=1;
+        for(int i=1;i<=num;i++) {
+        	fact= fact*i;
+        }
+        
+        System.out.println("Factorial of " + num + " is: " + fact);
+        sc.close();
+	}
+
+}
